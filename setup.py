eacc22493368b5e57beca488afcb47982c4a6f2a
Andy Lu <andy@stitchdata.com>
2025-07-02 18:30:31 +0000

    Bump versions
    Co-authored-by: Ben Allred <skuttleman@gmail.com>

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(name='tap-outbrain',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_outbrain'],
       install_requires=[
-          'singer-python==5.0.4',
-          'backoff==1.3.2',
-          'requests==2.20.0',
+          'singer-python==5.13.2',
+          'backoff==1.10.0',
+          'requests==2.32.4',
           'python-dateutil==2.6.0'
       ],
       entry_points='''
